9b2f5e8b96c9011ca69f3a9baa1945a702b4edbf
Chelsea Santoro <chels.santoro@gmail.com>
2026-03-14 14:38:37 -0400

    Add Blackbaud search to top prospect creation

--- apps/web/src/app/my-top-prospects/page.jsx
+++ apps/web/src/app/my-top-prospects/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useUser from "@/utils/useUser";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
